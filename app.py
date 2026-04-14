2bfd0d5b9d1b088670425e6c44d9ab15efefb8ac
Alexx131290 <145486954+Alexx131290@users.noreply.github.com>
2026-04-14 13:23:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+from datetime import timedelta  # <--  Para contar el tiempo de la sesion
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, session  # <-- Se Agrego 'session' a la importación
 
 from contenedores.home_controller import home
 from contenedores.cita_controller import cita
@@ -16,6 +17,35 @@ app = Flask(__name__, template_folder="vista", static_folder="static")
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "cambia-esta-clave-en-env")
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+
+# =====================================================================
+# PARCHE DE SEGURIDAD CLÍNICA (Aqui se evita que se guarde la Caché y que exista un Timeout)
+# =====================================================================
+
+# 1. Se definio que la sesión caduca automáticamente a los 10 minutos
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+
+@app.before_request
+def make_session_permanent():
+    """ 
+    Fuerza a que Flask controle el tiempo de la sesión en el servidor
+    sin importar la configuración de pestañas del navegador del usuario.
+    """
+    session.permanent = True 
+    app.permanent_session_lifetime = timedelta(minutes=10)
+
+@app.after_request
+def add_header(response):
+    """
+    Le prohíbe al navegador guardar el expediente en la memoria caché.
+    Si el doctor cierra sesión y le da "Atrás", le pedirá login otra vez.
+    """
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+# =====================================================================
 
 app.register_blueprint(home)
 app.register_blueprint(cita)
